06c90c983ab319ae1bd0f1e96714ffe669661178
Sinan <psydexx64@gmail.com>
2024-11-27 14:02:57 +0000

    Add Mediapipe hand detection and visualisation

--- hand_detection.py
+++ hand_detection.py
@@ -1,11 +1,22 @@
 import cv2
+import mediapipe as mp
 
 cap = cv2.VideoCapture(0)
+
+mp_hands = mp.solutions.hands.Hands(min_detection_confidence=0.7, min_tracking_confidence=0.5)
+mp_drawing = mp.solutions.drawing_utils
 
 while cap.isOpened():
     ret, frame = cap.read()
     if not ret:
         break
+
+    convert_to_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    results = mp_hands.process(convert_to_rgb)
+
+    if results.multi_hand_landmarks:
+        for hand_landmarks in results.multi_hand_landmarks:
+            mp_drawing.draw_landmarks(frame, hand_landmarks, mp.solutions.hands.HAND_CONNECTIONS)
 
     cv2.imshow("Webcam Feed", frame)
 
